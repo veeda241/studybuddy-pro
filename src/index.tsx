387e13dd71ab8e64edfaa6c1ab4d141fbf51edca
Vyas S <vyas.sk17@gmail.com>
2025-10-24 03:43:38 +0530

    Setup GitHub Actions workflow for deployment

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/studybuddy-pro">
       <AuthProvider>
         <App />
       </AuthProvider>
